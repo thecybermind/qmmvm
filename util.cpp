e93b71781a1f9a00ddf45294c0ab14f15d87972c
Kevin Masterson <cybermind@gmail.com>
2024-06-16 22:37:32 -0400

    updated license

--- util.cpp
+++ util.cpp
@@ -1,35 +1,13 @@
 /*
-
 QMMVM - VM Wrapper Tool
-Copyright QMM Team 2004
-http://www.quake3mm.net/
-
-Licensing:
-    QMM is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    QMM is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with QMM; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Copyright 2004-2024
+https://github.com/thecybermind/qmm/
+3-clause BSD license: https://opensource.org/license/bsd-3-clause
 
 Created By:
-    Kevin Masterson a.k.a. CyberMind <cybermind@users.sourceforge.net>
-
-Special Thanks:
-    VM code from Q3Fusion (http://www.sourceforge.net/projects/q3fusion/)
-    VM info from Phaethon (http://www.icculus.org/~phaethon/q3mc/q3vm_specs.html)
-    Code3Arena (http://www.planetquake.com/code3arena/)
+    Kevin Masterson < cybermind@gmail.com >
 
 */
-
-/* $Id: util.cpp,v 1.8 2004/08/30 00:45:36 cybermind Exp $ */
 
 #include "version.h"
 #include <stdio.h>
